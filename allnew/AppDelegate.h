c2f18636a41d388677e17d539d329df6ddf0be13
Martin Adam <martinadam@Martins-iMac.local>
2013-03-11 12:16:21 +0100

    Initial Commit

---
+++ allnew/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  allnew
+//
+//  Created by Martin Adam on 11.03.13.
+//  Copyright (c) 2013 mCRUMBS GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ allnew/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  allnew
+//
+//  Created by Martin Adam on 11.03.13.
+//  Copyright (c) 2013 mCRUMBS GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
